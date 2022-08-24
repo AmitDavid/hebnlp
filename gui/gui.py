b9eab0322e98991755d69a4aaaa07d181932e9c6
hadar-k <hadar.kanter@mail.huji.ac.il>
2022-08-24 22:27:57 +0300

    threading

--- gui/gui.py
+++ gui/gui.py
@@ -1,48 +1,110 @@
+from copyreg import pickle
 import PySimpleGUI as sg
-
+import os
 from pilmoji import Pilmoji
 from PIL import Image, ImageFont
+import pickle
+from threading import Thread, Lock
 
 
 def save_emoji_as_img(text):
     my_string = text
 
     with Image.new('RGB', (400, 80), (0, 0, 0)) as image:
-        font = ImageFont.truetype('arial.ttf', 50)
+        font = ImageFont.truetype('arial.ttf', 40)
 
         with Pilmoji(image) as pilmoji:
-            pilmoji.text((10, 10), my_string, (0, 0, 0), font)
+            pilmoji.text((10, 10), my_string, (255, 255, 255), font)
 
-            im1 = image.save("emoji_img.png", "PNG")
+            im1 = image.save("gui\emojiImg.png", "PNG")
 # ------------------------------------------------------------------------
 
+def load_model(file_name):
+    infile = open(file_name,'rb')
+    model = pickle.load(infile)
+    infile.close()
+    return model
 
-sg.theme('Black')
-layout = [[sg.Push(), sg.Image('gui/logo.png'), sg.Push()],
-          [sg.Push(), sg.Text('הכנס טקסט ולסיום לחץ אנטר', font='Arial 18', text_color='white'), sg.Push()],
-          [sg.Push(), sg.InputText(justification='r', font='Arial 18', key='-INPUT-', expand_y=True)],
-          [sg.Push(), sg.Image(key='-OUTPUT-'), sg.Push()],
-          [sg.Button('! עוד אחד', font='Arial 16'), sg.Push()]]
+# ------------------------------------------------------------------------
+def model_prediction(window, sentence, file_name):
+    path = "models\\" + file_name
+    # load_model
+    model = load_model(path)
+
+    #predict
+    predictions, raw_outputs = model.predict([sentence])
+    print(predictions)
+    # show result in GUI
+    window['-TemporarlyOutput-'].update(str(predictions.tolist()))
+
+    # important important 
+    # str_emoji = " 😁 💯"
+    # save_emoji_as_img(str_emoji)
+    # window['-OUTPUT-'].update('gui\emojiImg.png')
+    # window.write_event_value('-THREAD DONE-', '')
+
+# ------------------------------------------------------------------------
+def clean_choices(window):
+    window['-INPUT-'].update("")
+    window['-OUTPUT-'].update("")
+    window['-MODEL-'].update(default_model)
+    window['-TemporarlyOutput-'].update('')
+
+    
+# ------------------------------------------------------------------------
+def print_invalid_input(message, window):
+    window['-TemporarlyOutput-'].update(message)
+
+# ------------------------------------------------------------------------
+
+def initialize_layout():
+    layout = [[sg.Push(), sg.Image('gui/logo.png'), sg.Push()],
+        [sg.Push(), sg.OptionMenu(models_list, default_value=default_model, key='-MODEL-'), sg.Push()],
+                [sg.Push(), sg.Text('הכנס טקסט ולסיום לחץ אנטר', font='Arial 18', text_color='white'), sg.Push()],
+                [sg.Push(), sg.InputText(justification='r', font='Arial 18', key='-INPUT-', expand_y=True)],
+                [sg.Push(), sg.Image(key='-OUTPUT-'), sg.Push()],
+                [sg.Push(), sg.Text(key='-TemporarlyOutput-',font='Arial 18'), sg.Push()],
+                [sg.Button('! עוד אחד', font='Arial 16'), sg.Push()]]
+    
+    return layout
 
 
-window = sg.Window('התאמת אימוגי לטקסט בעברית', layout, return_keyboard_events=True, finalize=True)
-while True:
-    event, values = window.read()
-    if event == sg.WIN_CLOSED:
-        break
-    if event == '! עוד אחד':
-        window['-INPUT-'].update("")
-        window['-OUTPUT-'].update("")
+# ------------------------------------------------------------------------
+
+if __name__ == '__main__':
+
+    models_list = os.listdir('models')
+    default_model = "בחר מודל"
+
+    sg.theme('Black')
+    
+    layout = initialize_layout()
+
+    window = sg.Window('התאמת אימוגי לטקסט בעברית', layout, return_keyboard_events=True, finalize=True)
+    while True:
+        event, values = window.read()
+
+        if event == sg.WIN_CLOSED:
+            break
+
+        if event == '! עוד אחד':
+            clean_choices(window)
+
         # todo: האם \r הוא תמיד אנטר בכל מערכת.
-    if event == "\r":
-        str_emoji = "        😁 💯"
-        save_emoji_as_img(str_emoji)
+        if event == "\r":
 
-        window['-OUTPUT-'].update('gui/emojiImg.png')
+            # missing model
+            if (len(values['-MODEL-']) == 0) or (values['-MODEL-']==default_model):
+                print_invalid_input("אנא הכנס מודל", window)
 
-window.close()
+            # missing sentence
+            elif len(values['-INPUT-'].strip()) == 0:
+                print_invalid_input("אנא הכנס טקסט", window)
 
-# Hello, world! 👋 Here are some emojis: 🎨 🌊 😎
-# I also support Discord emoji: <:rooThink:596576798351949847>
+            # great, now predict!
+            else:
+                prediction_thread = Thread(target = model_prediction, daemon=True, args=(window, values['-INPUT-'], values['-MODEL-']))
+                prediction_thread.start()
 
-# print('You entered ', values[0])
+    window.close()
+
--- model/createModel.py
+++ model/createModel.py
@@ -1,41 +1,77 @@
 from simpletransformers.classification import ClassificationModel, ClassificationArgs
 import pandas as pd
 import logging
+import pickle
+import re
+from sklearn.model_selection import train_test_split
 
 
 logging.basicConfig(level=logging.INFO)
 transformers_logger = logging.getLogger("transformers")
 transformers_logger.setLevel(logging.WARNING)
 
-# Preparing train data
-train_data = [
-    ["Aragorn was the heir of Isildur", 1],
-    ["Frodo was the heir of Isildur", 0],
-]
-train_df = pd.DataFrame(train_data)
-train_df.columns = ["text", "labels"]
+LIST_OF_PATH_TO_CSV= {
+    "Kan11":r"C:\Users\User\Desktop\study\hebnlp\hebnlp\data\Kan11_comments.csv",
+   "Kan11News":r"C:\Users\User\Desktop\study\hebnlp\hebnlp\data\Kan11News_comments.csv",
+    "ScienceDavidson":r"C:\Users\User\Desktop\study\hebnlp\hebnlp\data\ScienceDavidson_comments.csv",
+}
 
-# Preparing eval data
-eval_data = [
-    ["Theoden was the king of Rohan", 1],
-    ["Merry was the king of Rohan", 0],
-]
-eval_df = pd.DataFrame(eval_data)
-eval_df.columns = ["text", "labels"]
+def reads_csv(path):
+    # read from csv
+    df = pd.read_csv(path,names=["text", "emoji"], encoding='utf-8' )
 
-# Optional model configuration
-model_args = ClassificationArgs(num_train_epochs=1)
+    # replace &quot; with ""
+    df['text'] = df['text'].apply(lambda x:re.sub('&quot;', '"', x))
 
-# Create a ClassificationModel
-model = ClassificationModel(
-    "roberta", "roberta-base", args=model_args
-)
+    # reverse text
+    # todo_1: problem with english..............
+    df['text'] = df['text'].apply(lambda x:x[::-1])
 
-# Train the model
-model.train_model(train_df)
+    return df
 
-# Evaluate the model
-result, model_outputs, wrong_predictions = model.eval_model(eval_df)
 
-# Make predictions with the model
-predictions, raw_outputs = model.predict(["Sam was a Wizard"])
+if __name__ == '__main__':
+
+    # load data from csv
+    df_davison = reads_csv(LIST_OF_PATH_TO_CSV["ScienceDavidson"])
+    df_Kan11 = reads_csv(LIST_OF_PATH_TO_CSV["Kan11"])
+    df_kan11News = reads_csv(LIST_OF_PATH_TO_CSV["Kan11News"])
+
+    # concat data and convert emojis to unique numbers 
+    all_data = pd.concat([df_davison, df_Kan11, df_kan11News])
+    all_data['labels'] = all_data.groupby(["emoji"]).ngroup()
+
+   #### smaller data frame - to delete later
+    num_of_classes = 20
+    all_data = all_data[all_data['labels']< num_of_classes]
+
+
+    # Preparing train data and eval data
+    X_train, X_test, y_train, y_test = train_test_split(all_data['text'], all_data['labels'], test_size=0.2, random_state=42)
+    train_df = pd.DataFrame({'text':X_train, 'labels':y_train})
+    eval_df = pd.DataFrame({'text':X_test, 'labels':y_test})
+   
+   
+    # Optional model configuration
+    # todo3 : I added output_dir="output_dir"
+    model_args = ClassificationArgs(num_train_epochs=1, output_dir="outputs", overwrite_output_dir=True)
+
+    # todo2 : use_cuda=False why??? doesnt work otherwise
+    # Create a ClassificationModel
+    model = ClassificationModel("roberta", "roberta-base", args=model_args,num_labels=num_of_classes, use_cuda=False)
+
+    # Train the model
+    model.train_model(train_df)
+
+    # Evaluate the model
+    # todo: 4 - need to save these parameters too!! 
+    result, model_outputs, wrong_predictions = model.eval_model(eval_df)
+
+    #save model
+    file_name = 'models\model_23.8'
+    outfile = open(file_name, 'wb')
+    pickle.dump(model, outfile)
+    outfile.close()
+
+    # Make predictions with the model
+    # predictions, raw_outputs = model.predict(["Sam was a Wizard"])
